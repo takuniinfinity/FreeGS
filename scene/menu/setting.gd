58f259702f2b6c692315c5d86d8f7a282a0d9ec1
takuniinfinity <takuniinfinity@gmail.com>
2023-10-24 21:19:19 +0700

    Add a change language feature in the menu scene

--- scene/menu/setting.gd
+++ scene/menu/setting.gd
@@ -1,5 +1,9 @@
 extends ScrollContainer
 
+var lang: Array = ["id", "en"]
 
 func _ready() -> void:
 	pass
+
+func _on_OptionButton_item_selected(idx) -> void:
+	TranslationServer.set_locale(lang[idx])
