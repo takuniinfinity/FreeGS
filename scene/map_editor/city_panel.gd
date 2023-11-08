102e087793ce75c85206bf08410e76bde5980c2a
takuniinfinity <takuniinfinity@gmail.com>
2023-11-08 07:02:07 +0700

    update map editor

--- scene/map_editor/city_panel.gd
+++ scene/map_editor/city_panel.gd
@@ -13,3 +13,6 @@ func _on_btn_del_pressed() -> void:
 
 func _on_city_panel_visibility_changed() -> void:
 	if visible: map_editor._terrain_change(0)
+
+func _on_name_text_changed(new_text: String) -> void:
+	map_editor.city_select.name = new_text
